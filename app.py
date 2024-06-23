f690afb1abac45f2fa27a4a051a1492dd895ef3b
Acheche_Achraf <achraf.acheche@etud.univ-jfc.fr>
2024-06-23 15:20:49 +0200

    cors settings

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
-from ocr import ocr_route
-from text_to_speech import tts_route
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+# Read CORS origins from environment variable
+cors_allowed_origins = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+
+CORS(app, resources={r"/api/*": {"origins": cors_allowed_origins}})
+
+# Register your blueprints
+from ocr import ocr_route
+from text_to_speech import tts_route
 
 app.register_blueprint(ocr_route)
 app.register_blueprint(tts_route)
